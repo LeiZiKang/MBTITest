ea1c6596fe25ce9c18164fe7c0c7baf3c4f22a2a
Kang <129756276+LeiZiKang@users.noreply.github.com>
2024-10-12 16:07:25 +0800

    实现基本需求

--- MBTITest/ContentView.swift
+++ MBTITest/ContentView.swift
@@ -50,70 +50,92 @@ struct ContentView: View {
     @State private var result: MBTIResult?
     @State var offset: CGFloat = .zero
     @State public var currentIndex: Int = 0
+    @State private var showResultView: Bool = false
     
     var body: some View {
         NavigationView {
-            HStack {
-                ForEach(presentViews()) { question in
-                    ZStack {
-                        Color.green
-                        VStack {
-                            Text(question.text)
-                                .font(.title)
-                            
-                            HStack{
-                                ForEach(question.options, id: \.self) { option in
-                                    Button(option) {
-                                        let answer = Answer(questionID: question.id, selectedOptionIndex: question.options.firstIndex(of: option)!)
-                                        if !self.answers.contains(where: { ans in
-                                            ans.questionID == answer.questionID
-                                        }){
-                                            self.answers.append(answer)
+            
+            VStack {
+                
+                Text("\(answers.count) / \(questions.count)")
+                
+                HStack {
+                    ForEach(presentViews()) { question in
+                        ZStack {
+                            Color.green
+                            VStack {
+                                Text(question.text)
+                                    .font(.title)
+                                
+                                HStack{
+                                    ForEach(question.options, id: \.self) { option in
+                                        Button(option) {
+                                            let answer = Answer(questionID: question.id, selectedOptionIndex: question.options.firstIndex(of: option)!)
+                                            if !self.answers.contains(where: { ans in
+                                                ans.questionID == answer.questionID
+                                            }){
+                                                self.answers.append(answer)
+                                            }
+                                            scrollNext()
                                         }
-                                        scrollNext()
+                                        .controlSize(.large)
+                                        .buttonStyle(.bordered)
+                                        .buttonBorderShape(.roundedRectangle)
+                                        .padding()
                                     }
-                                    .controlSize(.large)
-                                    .buttonStyle(.bordered)
-                                    .buttonBorderShape(.roundedRectangle)
-                                    .padding()
                                 }
                             }
                         }
+                        .frame(width: screenW - 40)
+                        .frame(maxHeight: .infinity)
+                        .cornerRadius(20)
                     }
-                    .frame(width: screenW - 40)
-                    .frame(maxHeight: .infinity)
-                    .cornerRadius(20)
                 }
+                .offset(x: offset)
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                            offset = value.translation.width
+                        })
+                        .onEnded({ value in
+                            var newIndex = 0
+                            // 向右拖动，显示上一张
+                            if value.translation.width > 50 {
+                                if currentIndex == 0 {
+                                    newIndex = questions.count - 1
+                                } else {
+                                    newIndex = currentIndex - 1
+                                }
+                            }
+                            // 向左拖动，显示下一张
+                            if value.translation.width < 50 {
+                                if currentIndex == questions.count - 1 {
+                                    newIndex = 0
+                                } else {
+                                    newIndex = currentIndex + 1
+                                }
+                            }
+                            
+                            changeBanner(newIndex: newIndex)
+                        })
+                )
+                .onChange(of: self.answers.count) { _, _ in
+                    if self.answers.count == self.questions.count {
+                        self.showResultView = true
+                    }
+                }
+                .sheet(isPresented: $showResultView) {
+                    
+                } content: {
+                    let result = MBTICalculator().calculate(answers: self.answers, questionBank: self.questions)
+                    ResultView(result: result)
+                }
+
+
             }
-            .offset(x: offset)
-            .gesture(
-                DragGesture()
-                    .onChanged({ value in
-                        offset = value.translation.width
-                    })
-                    .onEnded({ value in
-                        var newIndex = 0
-                        // 向右拖动，显示上一张
-                        if value.translation.width > 50 {
-                            if currentIndex == 0 {
-                                newIndex = questions.count - 1
-                            } else {
-                                newIndex = currentIndex - 1
-                            }
-                        }
-                        // 向左拖动，显示下一张
-                        if value.translation.width < 50 {
-                            if currentIndex == questions.count - 1 {
-                                newIndex = 0
-                            } else {
-                                newIndex = currentIndex + 1
-                            }
-                        }
-                        
-                        changeBanner(newIndex: newIndex)
-                    })
-            )
+ 
             .navigationBarTitle("MBTI 测试")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
     // 前台展示的3张图片
@@ -142,10 +164,6 @@ struct ContentView: View {
     }
 }
 
-// QuestionRow 视图用于展示单个题目
-//struct QuestionRow: View {
-//    // ...
-//}
 
 // ResultView 视图用于展示测试结果
 struct ResultView: View {
