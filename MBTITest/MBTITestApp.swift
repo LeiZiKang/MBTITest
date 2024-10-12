b4d8714f785edc6baa472ac54f4ee54ff22d2d07
Kang <129756276+LeiZiKang@users.noreply.github.com>
2024-10-12 13:24:40 +0800

    添加网络请求api

--- MBTITest/MBTITestApp.swift
+++ MBTITest/MBTITestApp.swift
@@ -6,12 +6,34 @@
 //
 
 import SwiftUI
+import Alamofire
 
 @main
 struct MBTITestApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: NSObject,UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+       
+        
+        return true
+    }
+    
+    
+}
+
+extension AppDelegate {
+    
+  
+
 }
