22c4f8898f415618a32c9515876d9e94d917228e
Kang <129756276+LeiZiKang@users.noreply.github.com>
2024-10-12 11:38:19 +0800

    Initial Commit

---
+++ MBTITest/MBTITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBTITestApp.swift
+//  MBTITest
+//
+//  Created by 雷子康 on 2024/10/12.
+//
+
+import SwiftUI
+
+@main
+struct MBTITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
